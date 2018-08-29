0c9d64e63567dfff2645f6a927191f272fcee8f2
str8lazy <str8lazy at gmail>
2018-08-28 22:49:49 -0500

    Did something in Powershell

---
+++ FizzBuzz.ps1
@@ -0,0 +1,11 @@
+﻿for ($i=1; $i -le 100; $i++){
+    if(($i%3 -eq 0)-and ($i%5 -eq 0)){
+        'FizzBuzz'
+        }
+    if($i%3 -eq 0){
+        'Fizz'
+        }
+    if($i%5 -eq 0){
+        'Buzz'
+        }
+    else{$i}}
